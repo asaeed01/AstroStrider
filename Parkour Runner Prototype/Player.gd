037c8ef3b591a66711d1bb88ad7654e907fabb5a
asaeed01 <asaeed@students.mags.school.nz>
2024-07-30 11:25:32 +1200

    Fixed Code

--- Parkour Runner Prototype/Player.gd
+++ Parkour Runner Prototype/Player.gd
@@ -6,25 +6,21 @@ const SPRINT_SPEED = 30
 const JUMP_VELOCITY = 9.5
 const SENSITIVITY = 0.004
 
-# Head Bob Variables
-const BOB_FREQ = 1
-const BOB_AMP = 0.08
-var t_bob = 0.0
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
 
 # FOV Variables
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes
-var gravity = 9.8
-
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
-
+# Crouching Variables
+var is_crouching = false
+var stand_height = 1.75
+var crouch_height = 1.0
+var current_height = stand_height
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -32,50 +28,57 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
-
 func _physics_process(delta):
-	
-	# Add the gravity
+	# Add gravity
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_crouching:
 		velocity.y = JUMP_VELOCITY
-	
-	# Handle Sprint.
-	if Input.is_action_pressed("sprint"):
+
+	# Handle Sprint
+	if Input.is_action_pressed("sprint") and not is_crouching:
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
 
-	# Get the input direction and handle the movement/deceleration
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2.0)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+
+	# Handle Crouch
+	if Input.is_action_pressed("crouch"):
+		is_crouching = true
+		current_height = crouch_height
+	else:
+		is_crouching = false
+		current_height = stand_height
+
+	# Adjust Camera Height
+	camera.transform.origin.y = lerp(camera.transform.origin.y, current_height, delta * 10.0)
+
+	# Get the input direction and handle the movement
 	var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if is_on_floor():
-		if direction:
+		if direction.length() > 0:
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+			velocity.x = lerp(velocity.x, 0.0, delta * 7.0)
+			velocity.z = lerp(velocity.z, 0.0, delta * 7.0)
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-	
-	# Head bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
-	
-	# FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	
+
+	# Debug information
+	print("Velocity:", velocity)
+	print("Direction:", direction)
+	print("Camera Position:", camera.transform.origin)
+
+	# Move and slide
 	move_and_slide()
 
-func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
-	return pos
